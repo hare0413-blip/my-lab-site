d19ae2c13782fe5f5a0bd621c9035de210061551
hare0413-blip <harenotikumori0413@gmail.com>
2026-01-08 00:21:06 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 const btn = document.getElementById("btn");
+const resetBtn = document.getElementById("resetBtn");
 const count = document.getElementById("count");
+const message = document.getElementById("message");
 
 let number = 0;
 
 btn.addEventListener("click", () => {
   number++;
   count.textContent = number;
+
+  if (number === 10) {
+    message.textContent = "🎉 10回達成！";
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  number = 0;
+  count.textContent = number;
+  message.textContent = "";
 });
